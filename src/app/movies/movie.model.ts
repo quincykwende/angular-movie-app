e10d70d74552d12f439942d91d0db8998556c3e2
Quincy Kwende <quincykwende@gmail.com>
2019-05-13 08:20:53 +0300

    added content + component list html

---
+++ src/app/movies/movie.model.ts
@@ -0,0 +1,16 @@
+export type GenreType = "action" | "adventure" | "biography" | "comedy" | "crime"
+	| "drama" | "history" | "mystery" | "scifi" | "sport" | "thriller";
+
+export const genreType = {
+	action: "action" as GenreType,
+	adventure: "adventure" as GenreType,
+	biography: "biography" as GenreType,
+	comedy: "comedy" as GenreType,
+	crime: "crime" as GenreType,
+	drama: "drama" as GenreType,
+	history: "history" as GenreType,
+	mystery: "mystery" as GenreType,
+	scifi: "scifi" as GenreType,
+	sport: "sport" as GenreType,
+	thriller: "thriller" as GenreType
+};
