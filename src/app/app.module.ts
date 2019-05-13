8bef3e55b2e55a78f7d39f5476cfd5ca6523e9e1
Quincy Kwende <quincykwende@gmail.com>
2019-05-13 11:00:39 +0300

    basic animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -18,7 +19,8 @@ import { MovieGenreComponent } from './movies/movie-genre/movie-genre.component'
     MovieGenreComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/movies/movie-list/movie-list.component.ts
+++ src/app/movies/movie-list/movie-list.component.ts
@@ -1,13 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, query } from '@angular/animations';
+
+import { movies } from '../movie.mock-data';
+import { MoviesComponent } from '../movies.component';
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.scss']
+  styleUrls: ['./movie-list.component.scss'],
+  animations: [
+    trigger('divState', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(500)
+      ]),
+      transition('* => void', [
+        animate(500, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ])
+    ]),
+  ]
 })
 export class MovieListComponent implements OnInit {
+  state = 'normal';
+  movies = movies;
 
   constructor() { }
+
+  onHover(){
+    //this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+  }
+
+  onClick(item) {
+
+  }
 
   ngOnInit() {
   }
